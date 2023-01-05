6c3e4517499379788714cd5b7ee3ca29ce338531
Madison Redd <madison.f.redd@gmail.com>
2023-01-05 12:10:39 -0500

    Added Scripts folder and Functions.cs
    No functions are optimized, as this is just a showcase

---
+++ Scripts/Functions.cs
@@ -0,0 +1,48 @@
+﻿namespace SubtractionAllTheWayDown
+{
+    class Functions
+    {
+        // a - b
+        public static int Subtract(int a, int b)
+        {
+            return 0 - ((0 - b) - (0 - a));
+        }
+
+        // a + b
+        public static int Add(int a, int b)
+        {
+            return Subtract(a, Subtract(0, b));
+        }
+
+        // a * b
+        public static int Multiply(int a, int b)
+        {
+            int v = 0;
+            for(int i = 0; i < a; Add(i,1))
+            {
+                v = Add(v, b);
+            }
+            return v;
+        }
+
+        // a / b
+        public static int Divide(int a, int b, out int r)
+        {
+            int v = 0;
+            int i = a;
+
+            if (a >= b)
+            {
+                while(i >= b)
+                {
+                    i = Subtract(i, b);
+                    v = Add(v, 1);
+                }
+            }
+
+            r = i;
+            
+            return v;
+        }
+    }
+}
